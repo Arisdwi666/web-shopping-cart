959b38ecf11bac91c7d8e852022f130a01be3793
Arisdwi666 <aristakiya000@gmail.com>
2022-07-11 16:38:13 +0700

    initial commit
    toko-laptop

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+
+const responsiveNavbar = (function () {
+	const button = document.querySelector("#menuButton");
+	const navbar = document.querySelector("#navbar")
+	button.addEventListener("click", function () {
+		if (navbar.className === "navbar") {
+			navbar.className += " navbarResponsive";
+		}
+		else {
+			navbar.className = "navbar";
+		}
+	});
+})();
+
+if (document.getElementById('hearderSlide')) {
+	$('#hearderSlide').multislider();
+	$('#hearderSlide').multislider('pause');
+}
+
+
+function closeCart() {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
+}
+
+
+const openShopCart = document.querySelector('.shoppingCartButton');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
+});
+
+
+const closeShopCart = document.querySelector('#closeButton');
+const overlay = document.querySelector('.overlay');
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
